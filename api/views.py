db76e6bd551aeb0f5da0e7da55344f8c89780e2d
Lucas <lucasfrutoszarate@hotmail.com>
2023-05-14 12:05:36 -0400

    using decorators

--- api/views.py
+++ api/views.py
@@ -1,16 +1,79 @@
 from django.shortcuts import render
 from . import models
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse, JsonResponse
 # importaciones necesarias para hacer api rest sin el framework djangorestframework
+from django.http import HttpResponse, JsonResponse
 import json
 # importaciones necesarias para utilizar el framework djangorestframework
 from . import serializer
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status # Posibly enum class with most of the http response with description so it is easier to find
 # Create your views here.
 
-@csrf_exempt #Anotacion para poder hacer peticiones post de manera segura en django
+#
+#   Starting the use of api_views decorator to simplify the code 
+#
+#   A decorator is a line of code that provides a function with certain methods, similar to extend a class
+#   but in a simplier way
+#
+#   Here we implement the Response class, this is usefull specially for test and debug, because it provides different type
+#   of output depending on the header accept content type that is used, and it also it abstract the logic of sending an 
+#   http response or a JsonResponse
+#
+
+@api_view(['GET', 'POST']) # in this case we need to specify the http method/s that we're going to use
 def peliculas(request): #hecho con django REST Framework
+    if request.method == 'GET':
+        peliculas = models.Pelicula.objects.all()
+        
+        #ordenar por query params
+        ordenar_por = request.GET.get('ordenarPor', '')
+        if ordenar_por:
+            peliculas = peliculas.order_by(ordenar_por)
+
+        respuesta = serializer.PeliculaSerializer(peliculas, many=True)
+
+        return Response(respuesta.data)
+    if request.method == 'POST':
+        serialized_data = serializer.PeliculaSerializer(data=request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT', 'DELETE']) 
+def pelicula(request, id):
+    try:
+        pelicula = models.Pelicula.objects.get(pk=id)
+    except Exception:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        respuesta = serializer.PeliculaSerializer(pelicula)
+        return Response(respuesta)
+    
+    if request.method == 'PUT':
+        serialized_data = serializer.PeliculaSerializer(pelicula, data=data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data) 
+
+    if request.method == 'DELETE':
+        pelicula.delete()      
+        return Response(status=status.HTTP_200_OK)
+
+    return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#
+#   Starting to use rest framework
+#
+
+@csrf_exempt #Anotacion para poder hacer peticiones post de manera segura en django
+def peliculas_rest_framework(request): #hecho con django REST Framework
     if request.method == 'GET':
         peliculas = models.Pelicula.objects.all()
         
@@ -35,7 +98,7 @@ def peliculas(request): #hecho con django REST Framework
     
 
 @csrf_exempt 
-def pelicula(request, id):
+def pelicula_rest_framework(request, id):
     try:
         pelicula = models.Pelicula.objects.get(pk=id)
     except Exception:
@@ -56,6 +119,11 @@ def pelicula(request, id):
         return HttpResponse(status=200)
 
     return JsonResponse(serialized_data.errors, status=400)      
+
+
+#
+#   Django Native Rest Service
+#
 
 
 @csrf_exempt #Anotacion para poder hacer peticiones post de manera segura en django
