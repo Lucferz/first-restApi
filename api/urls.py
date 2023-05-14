b5ed45d2c98e543dd6f6327762d13d6363fcc7d6
Lucferz25 <lucasfrutoszarate@hotmail.com>
2023-05-14 10:36:03 -0400

    endpoint single movie and add movie post

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('peliculas', views.peliculas),
+    path('peliculas/<int:id>', views.pelicula),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from . import models
-
+from django.views.decorators.csrf import csrf_exempt
 # importaciones necesarias para hacer http sin el framework
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 # Create your views here.
 
-
+@csrf_exempt #Anotacion para poder hacer peticiones post de manera segura en django
 def peliculas(request):
     if request.method == 'GET':
         peliculas = models.Pelicula.objects.all()
@@ -15,4 +16,43 @@ def peliculas(request):
             dict = {}
             dict['titulo'] = pelicula.titulo
             dict['sinopsis'] = pelicula.sinopsis
-        return HttpResponse(status=200)
+            dict['estreno'] = pelicula.estreno
+            respuesta.append(dict)
+
+        #return HttpResponse(status=200)
+        return JsonResponse(respuesta, safe=False)
+    if request.method == 'POST':
+        if request.content_type != 'application/json':
+            return HttpResponse(status=400) #bad request
+        
+        try:
+            jsonData = json.loads(request.body.decode())
+        except json.JSONDecodeError:
+            return HttpResponse(status=400)
+        
+        pelicula = models.Pelicula(
+            titulo = jsonData.get('titulo', ''),
+            estreno = jsonData.get('estreno', ''),
+            sinopsis = jsonData.get('sinopsis','')
+        )
+        respuesta ={
+            'id' : pelicula.pk,
+            'titulo' : pelicula.titulo,
+            'estreno' : pelicula.estreno
+        }
+
+        pelicula.save()
+        return JsonResponse(respuesta, status=201) #created
+
+
+def pelicula(request, id):
+    try:
+        pelicula = models.Pelicula.objects.get(pk=id)
+    except Exception:
+        return HttpResponse(status=404)
+    respuesta = {
+        "titulo" : pelicula.titulo,
+        "sinopsis" : pelicula.sinopsis
+    }
+
+    return JsonResponse(respuesta, safe=False)
